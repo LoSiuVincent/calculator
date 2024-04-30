61e2ec920bde508d0870ba86ec0d01c3c39f2041
Vincent Lo <lovincent536@gmail.com>
2024-05-01 02:54:13 +0800

    add multiply test

--- test_calculator.py
+++ test_calculator.py
@@ -1,5 +1,9 @@
-from calculator import add
+from calculator import add, multiply
 
 
 def test_add_returns_correct_result():
     assert add(1, 2) == 3
+
+
+def test_multiply_returns_correct_result():
+    assert multiply(1, 2) == 2
